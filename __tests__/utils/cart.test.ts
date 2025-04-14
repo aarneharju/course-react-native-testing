bafd3c3067515e2793490d20c12490db0faf6d1d
aarneharju <35223549+aarneharju@users.noreply.github.com>
2025-04-14 22:22:12 +0300

    Added module mocks for cart.test.ts
    Mocked Asyncstorage functions getItem and setItem.

--- __tests__/utils/cart.test.ts
+++ __tests__/utils/cart.test.ts
@@ -1,4 +1,10 @@
-import { getCartSum } from '@/utils/cart';
+import { getCartSum, loadCart, storeCart } from '@/utils/cart';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+jest.mock('@react-native-async-storage/async-storage', () => ({
+  setItem: jest.fn(),
+  getItem: jest.fn(),
+}));
 
 describe('Cart', () => {
   test('getCartSum', () => {
@@ -17,5 +23,55 @@ describe('Cart', () => {
       },
     ];
     expect(getCartSum(cart)).toBe(300);
+  });
+
+  test('Returns the correct item', async () => {
+    const cart = [
+      {
+        id: 1,
+        name: 'Product 1',
+        price: 100,
+        quantity: 2,
+      },
+      {
+        id: 2,
+        name: 'Product 2',
+        price: 200,
+        quantity: 1,
+      },
+    ];
+
+    storeCart(cart);
+    expect(AsyncStorage.setItem).toHaveBeenCalledWith(
+      'cart',
+      JSON.stringify(cart)
+    );
+
+    loadCart();
+    expect(AsyncStorage.getItem).toHaveBeenCalledTimes(1);
+  });
+
+  test('Loads the correct items', async () => {
+    const cart = [
+      {
+        id: 1,
+        name: 'Product 1',
+        price: 100,
+        quantity: 2,
+      },
+      {
+        id: 2,
+        name: 'Product 2',
+        price: 200,
+        quantity: 1,
+      },
+    ];
+
+    AsyncStorage.getItem = jest
+      .fn()
+      .mockResolvedValueOnce(JSON.stringify(cart)); // this overwrites the earlier mock
+    const result = await loadCart();
+
+    expect(result).toEqual(cart);
   });
 });
